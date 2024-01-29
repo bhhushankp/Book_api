531e303275fe5727d598cd3a6e8a6299f32e6eb6
Bhushanpatil <bhushanpatilbkp68@gmail.com>
2024-01-29 19:55:46 +0530

    .env added

--- pkg/config/db_connection.go
+++ pkg/config/db_connection.go
@@ -8,7 +8,7 @@ import (
 var DB *gorm.DB
 
 func ConnectDB() {
-	dsn := "root:@tcp(localhost:3306)/demo"
+	dsn := "root:@tcp(db:3306)/demo?charset=utf8mb4&parseTime=True&loc=Local"
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		panic("Failed to connect database")
